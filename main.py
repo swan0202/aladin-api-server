b5535220d61b7ffc549647403498e4d058b47744
swan0202 <kk22kk11kk@gmail.com>
2026-05-26 19:45:26 +0900

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import requests
-from bs4 import BeautifulSoup
 import re
 
 app = FastAPI()
@@ -55,43 +54,28 @@ def format_url(src):
 
 @app.get("/api/get-book-images")
 def get_book_images(item_id: str):
-    # 1. 네가 제보해준 도서 상세페이지(wproduct.aspx)를 메인 타겟으로 씁니다!
     url = f"https://www.aladin.co.kr/shop/wproduct.aspx?ItemId={item_id}"
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    html = response.text
     
     images = {"front": None, "spine": None, "back": None}
-    
-    # 2. 캡처 화면에서 확인한 HTML 클래스명으로 핀포인트 저격 🔫
-    c_front = soup.select_one('.c_front img')
-    c_spine = soup.select_one('.c_left img')
-    c_back = soup.select_one('.c_back img')
-    
-    if c_front and c_front.get('src'):
-        images['front'] = format_url(c_front.get('src'))
-    if c_spine and c_spine.get('src'):
-        images['spine'] = format_url(c_spine.get('src'))
-    if c_back and c_back.get('src'):
-        images['back'] = format_url(c_back.get('src'))
 
-    # 3. 혹시나 클래스명이 렌더링되지 않았을 경우를 대비한 2차 안전장치
-    # (네가 알려준 /spineflip/ 폴더와 _d.jpg 패턴을 정규식에 추가했어!)
-    if not images['spine'] or not images['back']:
-        all_urls = re.findall(r'(?:https?:)?//image\.aladin\.co\.kr/[^"\'\s>]*\.(?:jpg|png)', response.text, re.IGNORECASE)
-        
-        for src in all_urls:
-            src = format_url(src)
-            # 뒷표지 (_b.jpg)
-            if re.search(r'_(wbl|bl|b)\.jpg$', src, re.IGNORECASE) and not images['back']:
-                images['back'] = src
-            # 책등 (/spineflip/ 폴더 또는 _d.jpg)
-            elif re.search(r'(/spineflip/|_(sl|s|d)\.jpg$)', src, re.IGNORECASE) and not images['spine']:
-                images['spine'] = src
-            # 앞표지
-            elif re.search(r'_(wfl|fl|f)\.jpg$', src, re.IGNORECASE) and not images['front']:
-                images['front'] = src
+    # 💡 핵심: item_id가 포함되어야 한다는 조건을 지우고, 알라딘 이미지 주소를 전부 찾아냄
+    all_urls = re.findall(r'(?:https?:)?//image\.aladin\.co\.kr/product/[^"\'\s>]+(?:\.jpg|\.png)', html, re.IGNORECASE)
 
-    for k in images:
-        if not images[k]: images[k] = None
+    for src in all_urls:
+        src = format_url(src)
+
+        # 뒷표지: 캡처에서 본 letslook 폴더나 _b.jpg 우선 낚아채기
+        if re.search(r'(/letslook/|_(b|bl|wbl)\.jpg)$', src, re.IGNORECASE) and not images['back']:
+            images['back'] = src
+
+        # 책등: 캡처에서 본 spineflip 폴더나 _d.jpg 우선 낚아채기
+        elif re.search(r'(/spineflip/|_(d|s|sl)\.jpg)$', src, re.IGNORECASE) and not images['spine']:
+            images['spine'] = src
+
+        # 앞표지: 고화질 덮어쓰기용 백업
+        elif re.search(r'(/cover500/|_(f|wfl|2)\.jpg)$', src, re.IGNORECASE) and not images['front']:
+            images['front'] = src.replace("coversum", "cover500").replace("cover200", "cover500")
 
     return images
