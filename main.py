8159afb44e615c5e5298deaa73c993b7b5dc78ed
swan0202 <kk22kk11kk@gmail.com>
2026-07-19 10:07:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,12 @@ def search_books(query: str):
         "output": "js",
         "Version": "20131101"
     }
-    response = requests.get(url, params=params)
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+    }
+    
+    response = requests.get(url, params=params, headers=headers)
     
     try:
         data = response.json()
